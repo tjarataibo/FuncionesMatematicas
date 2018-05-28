7b279f43f502c707d4fa755e037abc174c94995b
Tamara <tjarataibo@gmail.com>
2018-05-28 13:46:15 -0400

    Js terminado

---
+++ app.js
@@ -0,0 +1,9 @@
+document.write("El resultado del siguiente problema matemático <strong>''36.325 * (9.824 + 777)''</strong> es: " + multiply(36325, add(9824, 777)));
+function add(numberOne, numberTwo) 
+{
+    return numberOne + numberTwo;
+}
+function multiply(numberThree, numberFour) 
+{
+    return numberThree * numberFour;
+}
